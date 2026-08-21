641482ffe5bb23b0191bff9138bc2afab50bf97c
Nanda Kishore <nandumallipeddi@gmail.com>
2026-08-21 15:20:38 +0530

    Fix server merge conflict

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,42 @@
-const UserRoutes = require("./routes/UserRoutes");
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
 const connectDB = require("./config/database");
-const tourRoutes = require("./routes/tourRoutes");
-<<<<<<< HEAD
+
 const UserRoutes = require("./routes/UserRoutes");
-=======
->>>>>>> eff7783d47b1abfb156b24a8cf78efa9d12ea16d
+const tourRoutes = require("./routes/tourRoutes");
 const wishlistRoutes = require("./routes/wishlistRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 const ticketRoutes = require("./routes/ticketRoutes");
+
 dotenv.config();
 
+// Connect to MongoDB
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// API Routes
 app.use("/api/users", UserRoutes);
+app.use("/api/tours", tourRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/ticket", ticketRoutes);
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Tour Travel Backend Running");
 });
 
-app.use("/api/tours", tourRoutes);
-
+// Port
 const PORT = process.env.PORT || 5000;
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
